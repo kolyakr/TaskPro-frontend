d6d8c0ed54dbeafba55c10d23d794f460899a9b5
Mykola Kryvoruchko <krivoruchko.kolya@gmail.com>
2024-12-26 09:40:19 +0200

    add images and icons

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import styles from "./App.module.css";
 
 const App: React.FC = () => {
-  return <>hello!</>;
+  return (
+    <div data-theme="light">
+      <div className={styles.testStyle}>hello!</div>
+      <p className={styles.testColorText}>this nis test color example</p>
+    </div>
+  );
 };
 
 export default App;
